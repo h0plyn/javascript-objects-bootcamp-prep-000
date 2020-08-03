508ecbeb1caf52e19fbba067cc3b565e4b5cdef1
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-03 21:41:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { "Da Baby": Ha };
 
 function updatePlaylist(obj, artistName, songTitle) {
-  return Object.assign({}, obj,{ artistName: songTitle})
+  return obj[artistName] = songTitle
 };
