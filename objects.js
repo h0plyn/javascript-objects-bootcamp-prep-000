71367cf42262556ca377137fdfa0d40a9d627db8
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-03 21:46:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { Future: "Big Rings" };
 function updatePlaylist(obj, artistName, songTitle) {
   return obj[artistName] = songTitle
 };
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName;
+};
